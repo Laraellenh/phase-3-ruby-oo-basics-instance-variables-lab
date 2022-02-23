2c7053f1ac006a3138293eb7a911cd349e9d4e3d
Lara <laraellenh@gmail.com>
2022-02-23 10:29:08 -0500

    @ before the instance variable so the other methods can see the info within the class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
-class Dog
 
+require "pry"
+
+class Dog 
+    # assigns name
+    def name=(name_given)
+        @this_dogs_name = name_given
+    end
+    # reports name
+    def name
+        @this_dogs_name
+    end
+
+lassie = Dog.new
+lassie.name = "Lassie"
 end
